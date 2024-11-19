b2c35377db8c830e15091ae18adba41a8ee6070c
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-11-19 15:43:53 +0530

    Javascript

---
+++ javascript/index.js
@@ -0,0 +1,50 @@
+var a;
+a = 30;
+
+// Number
+console.log(a, typeof a);
+
+// String
+a = "Javascript is Awesome";
+console.log(a, typeof a);
+
+// Boolean
+a = true;
+console.log(a, typeof a);
+
+a = undefined;
+console.log(a, typeof a);
+
+a = null;
+console.log(a, typeof a);
+
+// Number
+
+var b = 10;
+console.log(b); // Integer
+
+b = 10.111;
+console.log(b); // Float
+
+b = 123.1234567891;
+console.log(b); // Double
+
+b = 0x23ab;
+console.log(b);  // Hexa Decimal
+
+b = 0O123;
+console.log(b);  // Octal
+
+b = true;
+console.log(b); // Boolean
+
+b = false;
+console.log(b); // Boolean
+
+
+
+
+
+
+
+
