e5b83f915d6909e56b9798fb8d6a16c8c62bb60f
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-11-22 15:52:10 +0530

    String Methods

--- javascript/index.js
+++ javascript/index.js
@@ -1,86 +1,134 @@
 /**
- *  String 
- *  ------
- *      The Set of Characters Combined together to form a text is known as string
- *      Strings are represented in three ways 
- *          1. ''  (Single Quote)
- *          2. ""  (Double Quote)
- *          3. ``  (Backticks) / Template Literals
- *      How Many Ways to create String ? 
- *          1. '' , 2. "" 3. `` 4. new String();
-*       https://devdocs.io/javascript/
+ * Strings
+ * -------
+ *       '' , "" , `` , new String();
+ *   class X {
+ *      start(newTitle){
+            this.title = newTitle;
+ *      }
+ *   }
+ *      var ref = new X();
+ *      ref.start();
+ *      var str = 'Hello Wol'     
+ *      var str = new String("Hello World");
+ * 
+ *  What is Method ?
+ *  -----------------
+ *       Javascript Builtin - Methods
+ *       -----------------------------
+ *          The Methods are used to manipulate the object data.
+ *          The Methods are stored in properties of Object to perform
+ *          operations on same object
+ *              Ex : 
+ *                      var obj = {
+ *                              "name" : "Rahul",
+ *                              "print" : function(){
+ *                                  console.log(this.name);
+ *                              }
+ *                      }
+ *       There are two types of javascript in-built in methods 
+ *          1. non static methods
+ *                  The methods which are invoked by reference or literal are known 
+ *                  as non static methods
+ *                  Ex : 
+ *                          var str = "Hello World"; // Literal
+ *                          console.log(str.toUpperCase());
+ *                          
+ *                          var str = new String("Hello World"); // Dynamic in Nature
+ *                          console.log(str.toLowerCase()); 
+ *          2. static methods
+ *                  The Methods which are invoked by class name are known as static
+ *                  methods
+ *                      Ex : 
+ *                              console.log(Array.from("123"));
  */
+// declaration + assignment
+var a; // dec
+var str = "Hello World"; // initil
+// str.replace(target , newValue);
+console.log(str.replace("Hello", "HELLO"));
 
-// String literal
-var str1 = 'Hello World';
-// console.log(str1);
+str = "Hello World Hello World Hello World";
+console.log(str.replace("World", "There"));
+console.log(str.replaceAll("World", "There"));
+console.log(str);
 
-var str2 = "Hello World";
-// console.log(str2);
+console.log(str[0]);
+console.log(str.length);
+console.log(str[str.length - 1]);
+console.log(str[-1]);
 
-var str3 = `Hello World`;
-// console.log(str3);
+console.log(str.at(-1));
+console.log(str.at(5));
+console.log(str.at(-4));
+console.log(str.charAt(5));
+console.clear();
 
-// Dynamic Creation
-var str4 = new String("Hello World");
-// console.log(str4);
+console.log(str.charAt(20));
+console.log(str.charAt(45));
+console.log(str.charAt(-4));
+console.log(str.at(30));
 
-var str5 = "Hello";
-var str6 = "There";
+console.clear();
 
-var str7 = str5 + ' ' + str6;
-// Hello There 
-// console.log(str7);
+// https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
 
-var str8 = "Srinu";
-var str9 = "Chicken";
-var str10 = "Thumbsup";
+console.log(str.charCodeAt(14));
 
-// Srinu has ate chicken with combination of Thumbsup
+// Javascrip
+console.clear();
 
-var str11 = str8 + " has ate " + str9 + " with combination of " + str10;
-// console.log(str11);
+// console.log(str.slice(starting , ending));
+// console.log(str.slice(-4 , -1));
+// console.log(str.slice(4 , -5));
+// console.log(str.slice(0 , -12));
+// console.log(str.slice(4));
+// console.log(str.slice(4, 10));
+// console.log(str.slice(0,10));
+// Java is Awesome
+// console.log(str.slice(0,4) + str.slice(10));
+// console.log(str.slice(-2));
+// console.log(str.slice(-7));
 
-// I am srinu from hyderabad , i stay in hostel and on sundays in my hostel
-// chicken is available and i purchase thumbsup for kick
+// console.log(str.substring());
+// console.log(str.substring(4));
+// console.log(str.substring(4, 4));
+// console.log(str.substring(-1));
+// console.log(str.substring(-4));
+// console.log(str.substring(-6 , 5));
+// console.log(str.substring(5, 0));
+// console.log(str.substring(5 , -1));
 
-var str12 = 'I am ' + str8 + " from hyderabad , i stay in hostel and on sundays in my hostel "
-    + str9 + " is available and i purchase " + str10 + " for kick";
-// console.log(str12);
-
-// '' "" ``
-
-var str13 = `I am ${str8} from hyderabad , i stay in hostel and on sundays in my hostel ${str9} is available and i purchase ${str10} for kick`;
-// console.log(str13);
-
-var a = 10;
-var b = 20;
-var c = a + b;
-// The Sum of a and b is 30
-// console.log("The sum of a + b is", c);
-// console.log("The sum of a + b is", a + b);
-// console.log("The sum of a + b is " + c);
-// console.log("The sum of a + b is " + a + b);
-// console.log("The Sum of a + b is " + (a + b));
-// console.log(`The Sum of a + b is ${a + b}`);
+// console.log(str.substr());
+// console.log(str.substr(4 , 11));
+// console.log(str.substring(4));
+// console.log(str.slice(4));
+// console.log(str.substr(-1, 10));
 
 
+console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
 
-// console.log(2 + 2);   // 4
-// console.log(2 + "2");   // 22
-// console.log("100" - 2);  // 98
-// console.log("2" + 4);    // 24
-// console.log("2" + true);  // 2true
-// console.log("false" + true); // falsetrue
-// console.log(2 / "2");         // 1
-// console.log("2" + 2 + "10" + (100 + 30));  // 2210130
-// console.log("10 " + 10);   // 1010
-// console.log("2" * 2);             // error , 22 , 4 , 2
-// console.log("10" / -2);        // -5 , error 
-console.log("10" + 100 + +"100");  // error 10100100  100100 10100 10100 100 110100 10200
-console.log(10 + +"2" + 100);  // 102100
-console.log(10 + +"100" * 2);  // 20200 10200 
-console.log(typeof +"100");   // string ,  error , typeof100
-console.log(100 + + "100");
+console.log(str.split());
+console.log(str.split(" "));
+console.log(str.split(""));
+console.log(str.split("a"));
+console.log(str.split("", 5));
+console.log(str.split(" ", 1));
+console.clear();
+
+str = "javascript is awesome";
+// Javascript Is Awesome
+
+console.log(str.at(0).toUpperCase()
+    + str.slice(1, 11)
+    + str.at(11).toUpperCase()
+    + str.slice(12, 14)
+    + str.at(14).toUpperCase()
+    + str.slice(15));
+
+console.log(str.at(0).toUpperCase().concat(str.slice(1, 11)));
+
+
 
 
