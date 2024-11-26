5f1c623dc06bea3fe422fdc66d5e43c07f8a87ca
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-11-26 15:50:01 +0530

    Loops and Arrays

--- javascript/index.js
+++ javascript/index.js
@@ -1,66 +1,102 @@
 /**
- *  What is Array ?
- *  How to Access Elements from Array ?
- *  Why Index starts from 0 ?
- *  how to apply styles for console
- *  
- *      for loop
+ * For Loop and Arrays 
+ * --------------------
+ *          Syn :
+ *                  for(initialisation ; condition ; updation){
+ *                        // statements
+ *                  }  
+ *                  initialisation -> baby born
+ *                  condition      -> alive / death
+ *                  statements     -> cry , milk , sleep
+ *                  updation       -> ageing
  */
-// + - / * 
-console.log(10 + 10);
-console.log(20 + 30);
-console.log(20 < 30); // True
-console.log(20 >= 20); // True
-console.log(20 <= 30); // True
-console.log(20 == 20); // True (only checks the data)
-console.log(20 === 20); //  True    (checks the data and as well as type of data)
-console.log(20 == '20'); // False , true
-console.log(20 === '20'); // True  , False
-console.clear();
-
-var a = 10;
-var b = '10';
-console.log(a >= b); // f , t a > b , a == b
-console.log(a == b); // f , t
-console.log(a <= b); // f  , t a < b , a == b
-console.log(a > b);  // F
-console.log(a < b);  // f
-console.log(b > a);  // f
-console.log(b >= a);  //  f , t b > a  b == a
-console.log(b === a); // f  
-console.log(b == a);  // t
-console.clear();
-
-var a = 10;
-a = a + 1; // re assingment
-
-console.log(a); // 11
-
-a = a + 10;
-console.log(a);
-
-a += 10;
-console.log(a);
-
-console.clear();
-
 /**
- *   increment and decrement
- *   ------------------------
- *      post increment 
- *          suffix
- *      pre  increment
- *          prefix
- *      post decrement
- *          suffix
- *      pre decrement
- *          prefix
- *      ++ , --
- *      var a = 10;
- *      a++; 10++
+ * first iteration - only initialisation and condition
+ * if condition is true , statements are executed
+ * if condition is false , loop is terminated
+ * 
+ * from second iteration to end of loop
+ *          -> updation and condition 
+ *         if condition is true , statements are executed
+ *         if condition is false , loop is terminated  
+ * 
+ * 
+ * Array in Javascript
+ * -------------------
+ *      In javascript array is an object which can store any type of datatype without
+ *      any length restrictions in a single varaible
+ *      Always Array indexes starts from 0 
+ *      Arrays stores data in form of continous memory allocation
+ *      it can stores homogenous and hetrogenous data
+ *      There are several ways to create an Array
+ *      --------------------------------------------
+ *          1. using [] 
+ *          2. using new Array();
+ *          3. Array.from();
+ *        Syn : 
+ *                  variable refVar = [];
  */
-var x = 32; // 31
-var y = 12;  //  10
+var arr = [];
+console.log(arr, typeof arr);
+console.log(arr.length);
 
-//             32  +  32 + 32  + 12  + 12  - 12 - 31 + 12 + 12 + 10 + 31
-// console.log(x++ + --x + x-- + y++ + --y - y - x + y + y-- + --y + x++);
+console.clear();
+
+arr = [1, 2, 3, 4, 5, 6];
+console.log(arr);
+console.table(arr);
+console.log(arr.length);
+console.log(arr[0]);
+
+console.clear();
+
+arr[0] = 10;
+
+console.log(arr[0]);
+console.log(arr);
+
+console.clear();
+
+arr = [1, 2, 3, 4, 5, 6];
+
+arr[4] = arr[1] + arr[3];
+console.log(arr);
+
+console.log(arr[0]); // accessing first element
+console.log(arr[5]);
+console.log(arr.length);
+console.log(arr[arr.length - 1]); // accessing last element
+
+console.clear();
+
+
+arr = ['Devara', 'Akash', 'Krishna',
+    12, false, true, 3.0, 3.222222222,
+    NaN, undefined, {}, [], function () { }];
+
+console.log(arr);
+console.table(arr);
+
+console.clear();
+
+arr = ['Haritha', 'Durga', 'Ramu', 'PK', 'Sai', 'Pranathi', 'Sharuk'];
+
+console.log(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
+
+console.log("-----------------------")
+
+for (var i = 0; i <= 6; i++) {
+    console.log(arr[i]);
+}
+
+console.log("-----------------------")
+// 0 to 6
+for (var i = 0; i <= arr.length - 1; i++) {
+    console.log(arr[i]);
+}
+console.log("-----------------------")
+
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
