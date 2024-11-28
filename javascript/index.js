8bcda6f2af5bae34f7613a5a3f0ee1bb931c4e62
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-11-28 15:53:32 +0530

    Object

--- javascript/index.js
+++ javascript/index.js
@@ -1,117 +1,102 @@
-// Research -> join , concat , indexOf
-//              ..        -2   -1
-var arr = [10, 12, 15, 14, 67, 85];
-//         0    1   2   3  4   5
-console.log(arr.at(4));
-console.log(arr.at(0));
-console.log(arr.at(-1));
-console.log(arr[arr.length - 1]);
-console.log(arr.at(20));
+/**
+ *  Objects
+ * ----------
+ *      Object are used to store data in form of key and value pairs
+ *      Object in javascript is represented by {} i.e literal Object
+ *      keys in object should be always string (Recommended)
+ *      values in object can be any type of data 
+ *          Syn : 
+ *                  variable refVar = {};
+ *      key value pairs are seperated by , (Comma);
+ *      if there are duplicate keys in object , the value is assigned with last 
+ *      available key
+ *       
+ *      FAQ 
+ *      ---
+ *          How many Ways to Access Values from Literal Object
+ *              1. dot notation -> refVar.keyName
+ *              2. square notation (Recommended) -> refVar["keyname"];
+ *              3. Object.values(ref)
+ */
+var obj = {
+    "empName": "Shiva",
+    "tech": "JS",
+    "isAlive": true,
+    "empName": "Shiva Kumar",
+    "empName": "Shiva Reddy",
+    "empname": "Shiva Saithan",
+}
+// CRUD -> create , read , update , delete
+console.table(obj);
+console.log(obj.empName);
+console.log(obj['tech']);
+
+console.clear();
+console.log(`Hi , My Name is ${obj.empName} , I am Currently learning ${obj.tech} because i am ${obj.isAlive}`);
+console.log(`Hi , My Name is ${obj['empName']} , I am Currently learning ${obj['tech']} because i am ${obj['isAlive']}`);
+
+obj['gender'] = "Male"; // Adding Property to Exisiting object
+console.log(obj);
+
+obj['tech'] = "Javascript"; // Updating Existing Property of Object 
+console.log(obj);
+
+delete obj.empname; // deleting Existing property from object
+
+console.log(obj);
+
+var address = {
+    "state": "TS",
+    "city": "Hyderabad",
+    "pincode": "500042"
+}
+console.log(address);
+
+obj['address'] = address;
+
+console.log(obj);
+console.clear();
+console.log(obj.address); // Address Object , reading
+console.log(obj.address.state); // Ts
+console.log(obj['address'].pincode); // address.pincode
+console.log(obj.address['pincode']); // address['pincode']
+console.log(obj['address']['pincode']); // address['pincode]
 
 console.clear();
 
+var employee = {
+    'empName': "John",
+    empId: 100,
+    'address': {
+        'state': 'AP',
+        'capital': 'Amaravathi'
+    },
+    0: "employee"
+}
+console.log(employee);
+delete employee.empId;
+console.log(employee);
+console.log(employee.address.capital);
+// console.log(employee.0);
+console.log(employee['0']);
+
+
+var arr = [
+    {
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "rating": {
+            "rate": 3.9,
+            "count": 120
+        }
+    }
+]
+// Rate : 3.9 , Count : 120
 console.log(arr);
-arr.unshift(102);
-console.log(arr);
-var result = arr.unshift(30);  // 30 102 remaining -> 8
-console.log(arr);
-console.log(result);
-
-var res = arr.unshift(1, 2, 3, 4, 5);
-console.log(arr);
-console.log(res);
-
-console.clear();
-
-arr = [10, 14, 19, 12, 49];
-
-// arr[arr.length] = 20; // adding element at last
-
-arr.push(10);
-
-var res = arr.push(45, 18, 7);
-
-console.log(res); // 9
-console.log(arr);
-
-console.clear();
-
-arr = [10, 12, 94, 32, 14, 56];
-console.log(arr);
-arr.pop();   // 
-var res = arr.pop();
-console.log(arr); // 10 12 94 32 
-console.log(res);
-
-arr = [];
-res = arr.pop();
-console.log(res);
-
-console.clear();
-
-arr = [12, 4123, 312, 43, 34, 1, 12, 12];
-console.log(arr);
-arr.shift();
-res = arr.shift();
-console.log(arr, res);
-
-console.clear();
-
-arr = [12, 43, 12, 34];
-//    [0 , 0 , 0 , 0 ]
-
-// 0 - 3
-// for (var i = 0; i < arr.length; i++) {
-//     arr[i] = 0;
-//     console.log(arr);
-// }
-
-arr.fill(0);
-console.log(arr);
-
-arr = [12, 43, 12, 34];
-
-arr.fill(0, 1);
-console.log(arr);
-
-arr = [12, 43, 12, 34];
-arr.fill(0, 1, 3);
-console.log(arr);
-
-console.clear();
-
-//Slice and Splice
-
-arr = [12, 43, 54, 67, 8, 44, 2, 2, 4];
-console.log(arr.slice());
-console.log(arr.slice(2));
-console.log(arr);
-console.log(arr.slice(3, 7));
-console.log(arr.slice(-4));
-console.log(arr.slice(3, -2));
-console.log(arr.slice(0, -1));
-console.log(arr.slice(-14));
-console.log(arr.slice(-5, -10));
-console.clear();
-
-arr = [1, 4, 6, 1, 3, 4, 5, 9];
-res = arr.splice(1);
-console.log(arr);
-console.log(res);
-console.clear();
-arr = [2, 3, 45, 2, 4, 3, 21, 321, 3, 5, 5];
-arr.splice(0);
-console.log(arr);
-arr = [2, 3, 45, 2, 4, 3, 21, 321, 3, 5, 5];
-arr.splice(4, 1);
-console.log(arr);
-console.clear();
-arr.splice(6, 3);
-// 2 3 45 2 3 21 5
-console.log(arr);
-arr.splice(3, 1, 76);
-console.log(arr);
-arr.splice(3, 0, 34, 454, 65, 57, 67, 67);
-console.log(arr);
-
-console.clear();
+obj = arr[0];
+console.log(`Rate : ${arr[0].rating.rate} , Count : ${arr[0].rating.count}`);
+console.log(`Rate : ${obj.rating.rate} , Count : ${obj.rating.count}`);
