04c10662a2e903c95a8dc7d2291ec24f5001c35a
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-12-03 15:39:49 +0530

    Conditional Statements

--- javascript/index.js
+++ javascript/index.js
@@ -1,115 +1,103 @@
 /**
- *  Operators
- *  ---------
- *      Logical Operators
- *      ---------------
- *          Logical Operator in js are used to evaluate the Boolean Expression , where 
- *          whole expression is true or false based on give operator
- *          and operator , or operator , not operator
- *          && , || , !
- *          
- *          true && true -> true
- *          false && true -> false
- *          true && false -> false
- *          false && false -> false
- * -----------------------------------------------
- *         || 
- *         
- *          true || true -> true
- *          false || true -> true
- *          true || false -> true
- *          false || false -> false 
+ *     Conditional Statements in Javascript
+ *     -------------------------------------
+ *      Conditional Statements are used to perform/execute block of block based on 
+ *      condition 
+ *      1. simple if
+ *              syn : 
+ *                      if(condition){
+ *                      }
+ *      2. if else
+ *      3. ladder if
  */
-console.log(true || true);
-console.log(true || false);
-console.log(false || true);
-console.log(false || false);
+var srinivasHasLaptop = false;
+if (srinivasHasLaptop) {
+    console.log("Srinivas Can Attend Zoom Meeting");
+}
+if (!srinivasHasLaptop) {
+    console.log("Srinivas Cannot Attend Meeting");
+}
 
-console.log(10 == 10 || 11 != 11);
-console.log(10 > 10 || 9 != '9');
-console.log(10 > '10' || 9 !== '9');
-console.clear();
-
-var a = 10;
-console.log(a != 11 || ++a);
-console.log(a);
-
-console.clear();
-
-console.log(a < 9 || ++a);
-console.log(a);
-console.log((a > 10 && 10 == a) || a < 11);
-console.clear();
-
-
-console.log((a == 11 && a > 10) || ++a > 11);
-console.log(a);
-
-// ! not
-console.log(!true)
-console.log(!false);
-
-var a = 10;
-var b = 20;
-
-console.log((a > b) || (b == a + 10 && b == a));
-console.log(!(a > b));
+var grade = 'A';
+var percentage = 78;
+// if ((grade === 'A' || grade === 'B') && percentage > 75) {
+//     console.log("Eligible");
+// }
+// if (!((grade === 'A' || grade === 'B') && percentage > 75)) {
+//     console.log("Not Eligible");
+// }
+if ((grade === 'A' || grade === 'B') && percentage > 75) {
+    console.log("Eligible");
+}
+else {
+    console.log("Not Eligible");
+}
 
 console.clear();
+
+var a = 10, b = 5;
+if (a > b) {
+    console.log("A is Big");
+} else {
+    console.log("B is Big");
+}
+
+console.clear();
+
+var gender = 'Male';
+var age = 24;
+
+
+if (gender === 'Male' && age >= 21) {
+    console.log("Eligible For Ma");
+}
+else {
+    console.log("!");
+}
+
+
+console.clear();
+
 
 /**
- * Ternary Operator in Javascript
- *      Ternary Operator is used to check condition , ternary operator is short hand 
- *      if and else 
- *      if condition is true , it will execute true part which  is written after ?
- *      if condition is false , it will execute false part which is written after : 
- *      
- *      Syn :
- *              (condition) ? true part : false part; 
+ * Classify a person based on their age:
+- If the age is less than 13, they are a “Child”.
+- If the age is between 13 and 19 (inclusive), they are a “Teenager”.
+- Otherwise, they are an “Adult”.
  */
-var age = 18;
-(age >= 18) ? console.log("Eligible for Voting") : console.log("Not Eligible");
-var res = (age >= 18) ? "Eligible" : "!";
-console.log(res);
-
-// a = 10 , b = 11 -> if a > b -> a is big , b > a -> b is big
-
+var age = 23;
+if (age < 13) {
+    console.log("Child");
+}
+else if (age >= 13 && age <= 19) {
+    console.log("Teenager");
+}
+else {
+    console.log("Adult");
+}
 console.clear();
-var a = 11, b = 12;
-res = (a > b) ? "A is Big" : "B is Big";
-console.log(res);
+/**
+ * Assign a letter grade based on a student’s score:
+- Scores 90 and above: "A".
+- Scores between 80 and 89: "B".
+- Scores between 70 and 79: "C".
+- Scores below 70: "Fail".
+ */
 
-res = (b > a) ? "B is Big" : "A is Big";
-console.log(res);
-
-res = (a > b) ? "A is big" : (a < b) ? "B is big" : "both are  equal";
-console.log(res);
-
-var a = 10, b = 10, c = 10;
-// Find the largest b/w a , b , c
-
-res = (a > b && a > c) ? "A is big" : (b > c) ? "B is big" : "c is big";
-
-console.log(res);
-
-var res = (a > b && a > c) ? "a is greater"
-    : (b > a && b > c) ? "b is greater"
-        : (c > a && c > b) ? "c is greater" : "equal";
-console.log(res);
-
-res = (a > b && a > b) ? 'a is big' : (b > a && b > c) ? "b is big" : 'c is big';
-console.log(res);
-console.clear();
-
-a = 10, b = 20;
-console.log(`Before Swaping a = ${a} , b = ${b}`);
-
-c = a; // 10
-a = b; // 20
-b = c; // 10
-
-console.log(`After Swaping a = ${a} , b = ${b}`);
-
-// swap both numbers without using extra varaible
-
+var score = 65;
+if (score >= 90 && score <= 100) {
+    console.log("A");
+}
+else if (score >= 80 && score <= 89) {
+    console.log("B");
+}
+else if (score >= 70 && score <= 79) {
+    console.log("C")
+}
+else if (score >= 0 && score < 70) {
+    console.log("Fail");
+}
+else {
+    console.log("Invalid Marks")
+}
 
