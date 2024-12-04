986a0dcdbe54628a20f3c1a48a95ae1239dc6c8a
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-12-04 15:40:56 +0530

    Loops

--- javascript/index.js
+++ javascript/index.js
@@ -1,103 +1,80 @@
 /**
- *     Conditional Statements in Javascript
- *     -------------------------------------
- *      Conditional Statements are used to perform/execute block of block based on 
- *      condition 
- *      1. simple if
- *              syn : 
- *                      if(condition){
- *                      }
- *      2. if else
- *      3. ladder if
+ *  conditional statements
+ *  1. simple if
+ *  2. if else
+ *  3. ladder if
+ * 
+ *      i/p : HelloS
+ *      o/p : hELLOs
  */
-var srinivasHasLaptop = false;
-if (srinivasHasLaptop) {
-    console.log("Srinivas Can Attend Zoom Meeting");
+var str = "HelloThereRamu";
+var res = '';
+for (var i = 0; i < str.length; i++) {
+    if (str[i] == str[i].toUpperCase()) {
+        res = res + str[i].toLowerCase();
+    } else {
+        res = res + str[i].toUpperCase();
+    }
 }
-if (!srinivasHasLaptop) {
-    console.log("Srinivas Cannot Attend Meeting");
-}
+console.log(res);
 
-var grade = 'A';
-var percentage = 78;
-// if ((grade === 'A' || grade === 'B') && percentage > 75) {
-//     console.log("Eligible");
-// }
-// if (!((grade === 'A' || grade === 'B') && percentage > 75)) {
-//     console.log("Not Eligible");
-// }
-if ((grade === 'A' || grade === 'B') && percentage > 75) {
-    console.log("Eligible");
+
+// i/p -> yadhtrib // o/p -> birthday
+var str = 'inaylak yadhtrib yppah';
+// console.log(str.split('').reverse().join(''));
+res = '';
+for (i = str.length - 1; i >= 0; i--) {
+    res += str[i]; // res = res + str[i];
 }
-else {
-    console.log("Not Eligible");
-}
+console.log(res);
 
 console.clear();
-
-var a = 10, b = 5;
-if (a > b) {
-    console.log("A is Big");
+var obj = {
+    'number': 45
+}
+// if number property is even number print 1 - 10 even numbers , if it is odd number ,
+// print 1 - 10 odd numbers , if number is 0 , print 0
+if (obj.number == 0) {
+    console.log("0")
+}
+else if (obj.number % 2 == 0) {
+    for (i = 2; i <= 10; i += 2) {
+        console.log(i)
+    }
 } else {
-    console.log("B is Big");
+    for (i = 1; i <= 10; i += 2) {
+        console.log(i)
+    }
 }
 
 console.clear();
 
-var gender = 'Male';
-var age = 24;
-
-
-if (gender === 'Male' && age >= 21) {
-    console.log("Eligible For Ma");
-}
-else {
-    console.log("!");
+var obj = {
+    "name": "Pawan Kalyan",
+    "street": "KPHB",
 }
 
+// name => capital
+// {"name" : "PAWAN KALYAN" , "street" : "kphb"};
+
+obj.name = obj.name.toUpperCase();
+obj.street = obj.street.toLowerCase();
+console.log(obj);
 
 console.clear();
 
+obj = {
+    "name": "pAWAN", // PAWAN
+}
+// if name property having any small character , convert to uppercase
+for (var i = 0; i < obj.name.length; i++) {
+    console.log(i);
+    if (obj.name[i] == obj.name[i].toLowerCase()) {
+        obj.name = obj.name.toUpperCase();
+        break;
+    }
+}
+console.log(obj);
 
-/**
- * Classify a person based on their age:
-- If the age is less than 13, they are a “Child”.
-- If the age is between 13 and 19 (inclusive), they are a “Teenager”.
-- Otherwise, they are an “Adult”.
- */
-var age = 23;
-if (age < 13) {
-    console.log("Child");
-}
-else if (age >= 13 && age <= 19) {
-    console.log("Teenager");
-}
-else {
-    console.log("Adult");
-}
-console.clear();
-/**
- * Assign a letter grade based on a student’s score:
-- Scores 90 and above: "A".
-- Scores between 80 and 89: "B".
-- Scores between 70 and 79: "C".
-- Scores below 70: "Fail".
- */
 
-var score = 65;
-if (score >= 90 && score <= 100) {
-    console.log("A");
-}
-else if (score >= 80 && score <= 89) {
-    console.log("B");
-}
-else if (score >= 70 && score <= 79) {
-    console.log("C")
-}
-else if (score >= 0 && score < 70) {
-    console.log("Fail");
-}
-else {
-    console.log("Invalid Marks")
-}
 
