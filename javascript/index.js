76271ec09bcf9643ffea0940d5427c7945aae9ce
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-12-05 15:49:11 +0530

    Loops - 2

--- javascript/index.js
+++ javascript/index.js
@@ -1,80 +1,118 @@
 /**
- *  conditional statements
- *  1. simple if
- *  2. if else
- *  3. ladder if
- * 
- *      i/p : HelloS
- *      o/p : hELLOs
+ *  Loops in Javascript
+ *  -------------------
+ *      if(condition){
+ *              // statements
+ *      }
+ *      for(initilisation ; condition ; updation){  
+ *          
+ *      }
+ *      Types of Loops 
+ *      ---------------
+ *      1. for loop
+ *              for(ini ; condition ; updation){
+ *                  // statement
+ *              }
+ *      2. while loop
+ *              while(condition){
+ *                  // statements
+ *              }
+ *      3. do while loop
+ *      4. for...of loop  
+ *      5. for..in loop
  */
-var str = "HelloThereRamu";
-var res = '';
-for (var i = 0; i < str.length; i++) {
-    if (str[i] == str[i].toUpperCase()) {
-        res = res + str[i].toLowerCase();
-    } else {
-        res = res + str[i].toUpperCase();
-    }
-}
-console.log(res);
-
-
-// i/p -> yadhtrib // o/p -> birthday
-var str = 'inaylak yadhtrib yppah';
-// console.log(str.split('').reverse().join(''));
-res = '';
-for (i = str.length - 1; i >= 0; i--) {
-    res += str[i]; // res = res + str[i];
-}
-console.log(res);
-
-console.clear();
-var obj = {
-    'number': 45
-}
-// if number property is even number print 1 - 10 even numbers , if it is odd number ,
-// print 1 - 10 odd numbers , if number is 0 , print 0
-if (obj.number == 0) {
-    console.log("0")
-}
-else if (obj.number % 2 == 0) {
-    for (i = 2; i <= 10; i += 2) {
-        console.log(i)
-    }
-} else {
-    for (i = 1; i <= 10; i += 2) {
-        console.log(i)
-    }
-}
-
-console.clear();
-
-var obj = {
-    "name": "Pawan Kalyan",
-    "street": "KPHB",
-}
-
-// name => capital
-// {"name" : "PAWAN KALYAN" , "street" : "kphb"};
-
-obj.name = obj.name.toUpperCase();
-obj.street = obj.street.toLowerCase();
-console.log(obj);
-
-console.clear();
-
-obj = {
-    "name": "pAWAN", // PAWAN
-}
-// if name property having any small character , convert to uppercase
-for (var i = 0; i < obj.name.length; i++) {
+// for (i = 1; i <= a; i++)
+// {
+//     console.log(i);
+// }
+var i = 1;
+while (i <= 5) {
     console.log(i);
-    if (obj.name[i] == obj.name[i].toLowerCase()) {
-        obj.name = obj.name.toUpperCase();
-        break;
+    i++;
+}
+
+// question , print even number from 1 to 20 using while loop
+
+console.clear();
+var i = 2;
+while (i <= 20) {
+    console.log(i);
+    i = i + 2;
+}
+console.clear();
+// javascript , tpircsavaj -> while loop
+
+var str = "javascript";
+var res = '';
+var i = str.length - 1;
+while (i >= 0) {
+    res += str[i];
+    i--
+}
+
+console.log(res);
+
+console.clear();
+
+var arr = [1, 4, 3, 5, 3, 5, 6, 7, 8];
+// []; -> pop();
+
+var length = arr.length;
+while (length > 0) {
+    arr.pop();
+    length--;
+}
+console.log(arr);
+
+// debugger
+arr = ["kamal", "raghu", "rahul", "jansi", "Ramu", "Reshma"];
+for (var i = 0; i < arr.length; i++) {
+    var str = arr[i];
+    if (str[0] == 'r' || str[0] == 'R') {
+        arr[i] = str.toUpperCase();
     }
 }
-console.log(obj);
+console.log(arr);
 
+/**
+ * do while;
+ *      do{
+ *          // statements
+ *      }while(condition);
+ */
+
+var i = 1;
+while (--i > 0) {
+    console.log("while");
+}
+i = 1;
+do {
+    console.log("Do While");
+} while (--i > 0);
+
+console.clear();
+
+// 1 - 5 print
+
+var i = 1; // 2 3 4 5 6
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
+console.clear();
+
+var i = 1;
+do {
+    console.log(++i);
+} while (++i <= 10);
+
+console.clear();
+var i = 1; // 2 3 4 5 6 7 8
+do {
+    console.log(i + 3); // 4 5 6 7 8 9 10 11
+}
+while (i++ * 2 <= 14);
 
 
