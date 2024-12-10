eb6c8fd73ec55b1ea135fce5ebc55d7b04a7947a
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-12-10 15:42:50 +0530

    Functions with parameter and return

--- javascript/index.js
+++ javascript/index.js
@@ -1,89 +1,144 @@
-// Function Defination
-function print() {
-    console.log("Data printed");
+/**
+ *  Functions 
+ *  ---------
+ *      To perform specific operations
+ *      reusability
+ *      organise
+ *      Syn : 
+ *              function functionName([parameters]){
+ *                  // statements
+ *                  [return value]
+ *              }
+ *              functionName([arguments]);
+ */
+function maagie(maagiePacket) {
+    console.log(`Removed ${maagiePacket} and Cooked`);
 }
-// function Calling
-print();
+maagie("2 packets of maggie");
 
 console.clear();
 
-function goToKitchen() {
-    console.log("Entered Kitchen Room");
-    console.log("Some Work Done");
-    console.log("Exited from Kitchen Room");
+function generateBill(quantity, price) {
+    console.log(`Total Bill : ${price * quantity}`);
 }
-// console.log(goToKitchen);
-goToKitchen();
-console.log("Entered Bed Room");
+generateBill(2, 120);
 
 console.clear();
 
-function cooking() {
-    console.log("Enter Kitchen and Stared Cooking");
-    takeDelivery();
-    console.log("Completed Cooking");
+function sumOfTwoNumbers(a, b) {
+    console.log(`Sum of two numbers : ${a + b}`);
 }
-function takeDelivery() {
-    console.log("Exited From Kitchen");
-    console.log("Taken Delivery");
-    console.log("Back to Kitchen");
+sumOfTwoNumbers(10, 20);
+
+function modOfTwoNumbers(a, b) {
+    console.log(`Mod of two numbers: ${a % b}`);
 }
-cooking();
+modOfTwoNumbers(20, 5);
+
+console.clear();
+
+function sumOfThreeNumber(a, b, c) {
+    console.log(a + b + c);
+}
+sumOfThreeNumber(1, 2, 3);
 
 console.clear();
 
 
-function funTwo() {
-    console.log("Function Two Called");
+function funOne(a, b) {
+    console.log(a + "" + b);
+    funTwo(a, b);
 }
-function funOne() {
-    console.log("Function One Called");
-    funTwo();
-    console.log("Function One Completed");
-}
-funOne();
-funTwo();
-
-console.clear();
-
-three();
-function three() {
-    console.log("Third Function");
-}
-
-console.clear();
-
-function one() {
-    console.log("One");
-    two();
-    console.log("Three");
-}
-function two() {
-    console.log("Three");
-}
-function four() {
-    console.log("Two");
-    one();
-    console.log("One");
-}
-four();
-one();
-
-console.clear();
-
-function apple() {
-    var a = 20;
-    console.log(a++ + ++a + --a);
-    banana();
+function funTwo(a) {
     console.log(a);
 }
-function banana() {
-    var b = 12;
-    console.log(++b + b++);
-    cherry();
-    console.log("Bananaaaaaaaa");
+funOne(12, 14);
+
+console.clear();
+
+function one(a, b, c) {
+    console.log(a, b + c);
+    two(a + b, b + c);
+    console.log("one completed");
 }
-function cherry() {
-    console.log("Cherry.......");
+function two(a, b) {
+    console.log("two started");
+    console.log(a + b);
+    console.log("two ended");
 }
-apple();
+one(10, 30, 21);
+
+console.clear();
+
+function devara(hero, mainCharacter) {
+    console.log("Devara");
+    pushpa(hero + mainCharacter);
+    console.log(hero, mainCharacter);
+    pushpa("Allu Arjun", "PK");
+}
+function pushpa(hero, villian) {
+    console.log("Pushpa");
+    console.log(hero + " " + villian);
+}
+
+function pushpa(hero) {
+    console.log(hero);
+}
+
+function pushpa() {
+    console.log("Hey");
+}
+
+devara("NTR", "Prakash Raj");
+
+
+function x(a, b, c, d) {
+    console.log(a, b, c, d);
+}
+x(10, 20);
+
+console.clear();
+
+// return keyword
+function print() {
+    return "Printed";
+}
+var result = print();
+console.log(result);
+
+console.log(print());
+
+/**
+ * if function is returning a value then we need
+ * to store the data in any variable or just print 
+ * it
+ * 
+ * Syn : 
+ *       varaible identifier =  functionName([arguments]);
+ *       console.log(functionName([arguments]));
+ */
+
+console.clear();
+
+function demo() {
+
+}
+console.log(demo());
+// if any function is not returning a value
+// then output will be undefined
+
+console.clear();
+
+function sample(a) {
+    return a + 10;
+}
+var result = sample(10);
+// console.log(result);
+// console.log(sample(10));
+
+
+function sumOfTwoNumbers(a, b) {
+    return (a + b);
+}
+var sumResult = sumOfTwoNumbers(10, 20) ;
+console.log(sumResult);
