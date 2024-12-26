b5d095fdce262d01adde5e19534b0bc6ca5417f5
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-12-26 15:25:28 +0530

    delete all

--- javascript/index.js
+++ javascript/index.js
@@ -2,18 +2,17 @@
  *    fetch(URL , options);
  */
 let options = {
-    "method": "PATCH",
-    "headers": {
-        "Content-Type": "application/json"
-    },
-    "body": JSON.stringify({
-        "id": "4",
-        "name": "donkey"
-    })
+    "method": "DELETE"
 }
-fetch("http://localhost:5000/data/4", options)
-    .then(resp => {
-        if (resp.ok) {
-            console.log(resp.statusText);
+fetch("http://localhost:5000/data/")
+    .then(respose => respose.json())
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            fetch(`http://localhost:5000/data/${data[i].id}`, options)
+                .then(res => {
+                    if (res.ok) {
+                        console.log("deleted : ", res.statusText);
+                    }
+                })
         }
-    }).catch(err => console.error(err));
+    })
