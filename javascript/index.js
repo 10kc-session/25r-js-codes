7ff42c15b3561a24020acd7fd0426c6bb514936f
10kc-session <165980049+10kc-session@users.noreply.github.com>
2024-12-26 15:46:14 +0530

    Add All Data At a time

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,46 @@
-/**
- *    fetch(URL , options);
- */
+let data = [
+    {
+        "id": 1,
+        "todo": "Do something nice for someone you care about",
+        "completed": false,
+        "userId": 152
+    },
+    {
+        "id": 2,
+        "todo": "Memorize a poem",
+        "completed": true,
+        "userId": 13
+    },
+    {
+        "id": 3,
+        "todo": "Watch a classic movie",
+        "completed": true,
+        "userId": 68
+    },
+    {
+        "id": 4,
+        "todo": "Watch a documentary",
+        "completed": false,
+        "userId": 84
+    }
+]
+
+
 let options = {
-    "method": "DELETE"
+    "method": "POST",
+    "headers": {
+        "Content-Type": "application/json"
+    }
 }
-fetch("http://localhost:5000/data/")
-    .then(respose => respose.json())
-    .then(data => {
-        for (let i = 0; i < data.length; i++) {
-            fetch(`http://localhost:5000/data/${data[i].id}`, options)
-                .then(res => {
-                    if (res.ok) {
-                        console.log("deleted : ", res.statusText);
-                    }
-                })
-        }
-    })
+let url = "http://localhost:5000/data/"
+
+for (let i = 0; i < data.length; i++) {
+    let obj = data[i];
+    options.body = JSON.stringify(obj);
+    fetch(url, options)
+        .then(res => {
+            if (res.ok) {
+                console.log(res.statusText);
+            }
+        })
+}
