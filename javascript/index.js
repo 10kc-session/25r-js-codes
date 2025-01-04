b9c4e2e96167d83e4421fbe2a993427374f1e582
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-01-04 15:27:20 +0530

    With Styles

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,8 @@
-async function getData() {
+let getData = async () => {
     try {
-        let response = await fetch("http://localhost:5000/products", { method: "GET" });
+        let response = await fetch("http://localhost:5000/products");
         if (!response.ok) {
-            throw new Error("Data Not Found");
+            throw new Error(response.status + " " + response.statusText);
         }
         let data = await response.json();
         printData(data);
@@ -10,59 +10,40 @@ async function getData() {
         alert("Data failed to fetch");
     }
 }
-
 function printData(data) {
-    // creating a table tag
     let table = document.createElement("table");
 
-    // Heading
-
-    // creating a thead tag to store heading
+    // Head
     let thead = document.createElement("thead");
-
-    // creating a heading row to append to thead
     let trhead = document.createElement("tr");
-
-    // reducing boiler plate with array and forEach 
     let headings = ["id", "title", "price", "description", "category", "image"];
     headings.forEach(ele => {
         let th = document.createElement("th");
         th.innerText = ele;
         trhead.appendChild(th);
-    });
-    // Appending trhead to thead tag to display headings    
+    })
     thead.appendChild(trhead);
+    // End of Head
 
     // Body
-    // creating tbody tag to store the data
-    let tbody = document.createElement("tbody");
 
-    // iterating over data came for api using forEach 
+    let tbody = document.createElement("tbody");
     data.forEach(obj => {
         let tr = document.createElement("tr");
         for (let key in obj) {
-            // Skipping Rating Object from the Real Object
             if (key !== "rating") {
                 let td = document.createElement("td");
                 if (key === "image") {
-                    let img = document.createElement("img");
-                    img.src = obj[key];
-                    td.appendChild(img);
+                    td.innerHTML = `<img src=${obj[key]}>`
                 } else {
                     td.innerText = obj[key];
                 }
-                // appending td to tr
                 tr.appendChild(td);
             }
         }
-        // appending tr to tbody , which is having data 
         tbody.appendChild(tr);
     })
-
-    // appending thead and tbody to table tag 
     table.append(thead, tbody);
-
-    // appending table to body tag to display data
     document.body.appendChild(table);
 }
 getData();
