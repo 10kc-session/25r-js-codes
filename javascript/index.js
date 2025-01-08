1686702d215dc0617f9aa7af072df0d968b215ba
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-01-09 00:23:58 +0530

    admin

--- javascript/index.js
+++ javascript/index.js
@@ -1,42 +1,78 @@
-let btn = document.getElementById("btn");
-btn.addEventListener("click", async () => {
-    console.log("getting the data");
-    let response = await fetch("http://localhost:5000/products");
+async function fetchData() {
+    let res = await fetch("http://localhost:5000/products");
+    let products = await res.json();
+    displayData(products);
+}
+function displayData(products) {
+    let grid_container = document.querySelector(".grid-container");
+    grid_container.innerHTML = products.map(product => {
+        return `<h3>${product.title}</h3>
+        <p>${product.description}</p>
+        <p>${product.price}</p>
+        <button class = "editButton" onclick="editData('${product.id}')">Edit</button>
+        <button class="deleteButton" onclick="deleteData('${product.id}')">Delete</button>`
+    }).join('');
+}
+
+async function editData(id) {
     try {
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
+        let response = await fetch(`http://localhost:5000/products/${id}`);
+        let product = await response.json();
+        let feilds = ["title", "price", "description", "category", "image"];
+        feilds.forEach(feild => document.getElementById(feild).value = product[feild]);
+        document.getElementById("id").value = product.id;
+        document.getElementById("rate").value = product.rating.rate;
+        document.getElementById("count").value = product.rating.count;
     } catch (error) {
+        alert("Something Went Wrong");
         console.error(error);
     }
-    let data = await response.json();
-    displayData(data);
-})
-
-function displayData(data) {
-    let container = document.getElementsByClassName("container")[0];
-    data.forEach(function (obj, index) {
-        let item = document.createElement("div");
-        item.innerHTML = `
-                <img src='${obj.image}'>
-                <p>${obj.title}</p>
-                <p>${obj.description}</p>
-                <button id = "btn-${index}">Remove Element</button>
-        `;
-        // let button = document.createElement("button");
-        // button.innerText = "Remove";
-        // button.onclick = () => {
-        //     item.remove();
-        // }
-        // item.appendChild(button);
-        container.appendChild(item);
-        let button = document.getElementById(`btn-${index}`);
-        button.onclick = () => {
-            item.remove();
+}
+async function submit() {
+    let id = document.getElementById("id").value;
+    const obj = {
+        "title": document.getElementById("title").value,
+        "price": document.getElementById("price").value,
+        "description": document.getElementById("description").value,
+        "category": document.getElementById("category").value,
+        "image": document.getElementById("image").value,
+        "rating": {
+            "rate": document.getElementById("rate").value,
+            "count": document.getElementById("count").value
         }
     }
-    )
+    const URL = "http://localhost:5000/products";
+    let url = id ? `${URL}/${id}` : URL;
+    let methodValue = id ? "PUT" : "POST";
+    await fetch(url, {
+        "method": methodValue,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": JSON.stringify(obj)
+    })
+    fetchData();
 }
-function deleteData(arr, index, item) {
-    console.log(item);
+
+
+async function deleteData(id) {
+    let confirm = window.confirm("Do you really want to delete ? ");
+    if (confirm) {
+        let response = await fetch(`http://localhost:5000/products/${id}`, { "method": "DELETE" });
+        try {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            alert("Data Deleted");
+            fetchData();
+        } catch (error) {
+            alert("Something Went Wrong");
+            console.error(error);
+        }
+    }
+    else {
+        alert("Aborted");
+    }
 }
+
+document.addEventListener("DOMContentLoaded", fetchData);
