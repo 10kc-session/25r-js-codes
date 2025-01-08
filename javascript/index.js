742f5d218d63ca997ded64f2dd170adb80a6ef72
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-01-08 15:45:54 +0530

    Delete

--- javascript/index.js
+++ javascript/index.js
@@ -1,33 +1,42 @@
-function getWeather() {
-    let city = document.getElementById("cityName");
-    showWeather(city.value);
-}
-function displayData(result) {
-    let loader = document.getElementById("loader");
-    loader.style.display = "none";
-    let item = document.getElementById("showW   eather");
-    item.innerHTML =
-        `<p>City : ${result.name}</p>
-        <p>Temp : ${result.main.temp}°F</p>
-        `
-}
-async function showWeather(city) {
-    const url = `https://open-weather13.p.rapidapi.com/city/${city}/EN`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': 'b126d85474mshe64f1aa4d8742e9p19b32ajsn2a2e19b22508',
-            'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
-        }
-    };
+let btn = document.getElementById("btn");
+btn.addEventListener("click", async () => {
+    console.log("getting the data");
+    let response = await fetch("http://localhost:5000/products");
     try {
-        const response = await fetch(url, options);
         if (!response.ok) {
             throw new Error(response.statusText);
         }
-        const result = await response.json();
-        displayData(result);
     } catch (error) {
         console.error(error);
     }
+    let data = await response.json();
+    displayData(data);
+})
+
+function displayData(data) {
+    let container = document.getElementsByClassName("container")[0];
+    data.forEach(function (obj, index) {
+        let item = document.createElement("div");
+        item.innerHTML = `
+                <img src='${obj.image}'>
+                <p>${obj.title}</p>
+                <p>${obj.description}</p>
+                <button id = "btn-${index}">Remove Element</button>
+        `;
+        // let button = document.createElement("button");
+        // button.innerText = "Remove";
+        // button.onclick = () => {
+        //     item.remove();
+        // }
+        // item.appendChild(button);
+        container.appendChild(item);
+        let button = document.getElementById(`btn-${index}`);
+        button.onclick = () => {
+            item.remove();
+        }
+    }
+    )
+}
+function deleteData(arr, index, item) {
+    console.log(item);
 }
