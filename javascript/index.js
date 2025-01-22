42e6ae0131586552872331c2e1b5ce4ea1e1c918
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-01-22 15:56:17 +0530

    BOM

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,23 @@
-let input = document.getElementById("text");
-let error = document.getElementsByClassName("error-message")[0];
-input.addEventListener("input", () => {
-    console.log(input.value);
-    if (input.value.length <= 3) {
-        error.innerText = "Should be more than 3"
-        error.classList.add("error-message");
+function saveData() {
+    let input = document.querySelector("input");
+    let arr = JSON.parse(localStorage.getItem("name")) || [];
+    arr.push(input.value);
+    localStorage.setItem("name", JSON.stringify(arr));
+    input.value = ''
+    displayData();
+}
+function displayData() {
+    let container = document.getElementsByClassName("container")[0];
+    container.innerHTML = ``;
+    let arr = JSON.parse(localStorage.getItem("name"));
+    if (arr === null) {
+        container.innerHTML = "no data available";
     } else {
-        error.innerText = "";
-        error.classList.remove("error-message");
+        arr.forEach(name => {
+            let p = document.createElement("p");
+            p.innerText = name;
+            container.appendChild(p);
+        })
     }
-})
+}
+window.onload = displayData;
