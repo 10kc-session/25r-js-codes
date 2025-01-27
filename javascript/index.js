7e4332d80020d52691d7435393095a7dc8475c32
10kc-session <165980049+10kc-session@users.noreply.github.com>
2025-01-27 15:45:25 +0530

    SET

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,40 @@
-let input1 = document.getElementById("name");
-let input2 = document.getElementById("pass");
-let nameError = document.getElementById("nameError");
-let passError = document.getElementById("passError");
-document.getElementById("form").addEventListener("submit", (event) => {
-    if (input1.value === '' || !(/^(?=.*[a-z]).{3,}$/.test(input1.value))) {
-        input1.classList.add("error");
-        nameError.innerHTML = "<i style = 'color : red'>username required</i>";
-        event.preventDefault();
-    } else {
-        nameError.innerHTML = ``
-        input1.classList.remove("error");
-    }
-    if (input2.value === '' || !(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,}$/.test(input2.value))) {
-        input2.classList.add("error");
-        passError.innerHTML = "<i style = 'color : red'>password required</i>"
-        event.preventDefault();
-    }
-    else {
-        passError.innerHTML = ``;
-        input2.classList.remove("error");
-    }
-})
+// let arr1 = [1, 3, 2, 1, 3, 2, 3];
+// let arr2 = [1, 3, 2, 1, 3, 2, 3];
+let obj1 = {
+    "name": "rahul"
+}
+let obj2 = {
+    "name": "rahul"
+}
+let set = new Set();
+set.add(obj1.name);
+set.add(obj2.name);
+console.log(set);
+console.log(set.has("rahul"));
+set.add(1);
+set.add(3);
+set.add(5);
+console.log(set);
+console.log(...set);
+set.forEach(value => console.log(value));
+
+let males = ["ravi", "rahul", "ravi", "rahul", "rishi", "rahul"];
+let females = ["rani", "ramya", "rani", "rani", "ramya"];
+
+// ravi , rahul , rishi , rani , ramya
+let gender = [...new Set(males), ...new Set(females)];
+console.log(gender);
+
+let setA = new Set([1, 2, 3, 4, 5, 6]);
+let setB = new Set([4, 5, 6, 7]);
+
+res = [...setB].filter(ele => setA.has(ele));
+
+console.clear();
+console.log(res);
+
+set.clear();
+
+console.log(set);
+console.log(set.size);
+// ---------------------------------------------------------
